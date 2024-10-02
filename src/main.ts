1bdcf20bfe69e580fb6ad9d219d358def92f768d
aldruin <abl.souza97@gmail.com>
2024-10-02 19:44:07 -0300

    ::feat:: Configura API Express com rotas de criação e listagem
    - Implementa função main para inicializar a API
    - Adiciona integração com Prisma via Repository
    - Configura UseCases (services) para gerenciar a entidade
    - Define e registra rotas de criação e listagem
    - Inicia o server na porta 8000

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,27 @@
-console.log("Hello world!")
+import { ApiExpress } from "./infra/api/express/api.express";
+import { CreateProductRoute } from "./infra/api/express/routes/product/create-product.express.route";
+import { ListProductRoute } from "./infra/api/express/routes/product/list-product.express.route";
+import { ProductRepositoryPrisma } from "./infra/repositories/product/product.repository.prisma";
+import { prisma } from "./package/prisma/prisma";
+import { CreateProductUsecase } from "./usecases/product/create/create-product.usecase";
+import { ListProductUsecase } from "./usecases/product/list/list-product.usecase";
+
+function main(){
+
+    const aRepository = ProductRepositoryPrisma.create(prisma);
+
+    const createProductUsecase = CreateProductUsecase.create(aRepository);
+    const listProductUsecase = ListProductUsecase.create(aRepository);
+
+    const createRoute = CreateProductRoute.create(createProductUsecase);
+    const listRoute = ListProductRoute.create(listProductUsecase);
+
+
+    const api = ApiExpress.create([createRoute, listRoute]);
+
+    const port = 8000;
+    api.start(port);
+
+}
+
+main();
